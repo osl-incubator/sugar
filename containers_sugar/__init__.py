36fedc15fdb6be401c89c215ad4193d0dd7b94a8
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-01-20 20:06:47 -0400

    fix: semantic-release replace plugin configuration

--- containers_sugar/__init__.py
+++ containers_sugar/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Ivan Ogasawara'
 __email__ = 'ivan.ogasawara@gmail.com'
-__version__ = '1.0.0'
+__version__ = '1.0.0'  # semantic-release
 
 
 from containers_sugar.sugar import Sugar  # noqa: F401
