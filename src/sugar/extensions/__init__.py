df91c0884ad062856d68e8b448a8b0d361fd2237
SanjayUG <ganjekarsanjay6@gmail.com>
2025-03-17 22:30:25 +0530

    feat(extensions): add podman-compose extension

--- src/sugar/extensions/__init__.py
+++ src/sugar/extensions/__init__.py
@@ -1,1 +1,15 @@
 """Plugins module."""
+
+from sugar.extensions.base import SugarBase
+from sugar.extensions.compose import SugarCompose
+from sugar.extensions.compose_ext import SugarComposeExt
+from sugar.extensions.podman_compose import SugarPodmanCompose
+from sugar.extensions.stats import SugarStats
+
+__all__ = [
+    'SugarBase',
+    'SugarCompose',
+    'SugarComposeExt',
+    'SugarPodmanCompose',
+    'SugarStats',
+]
