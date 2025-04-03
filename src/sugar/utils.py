12b57134d1a93075972b9daf62086d7deb5e8be2
Sai Sanjay <97831658+sanjay7178@users.noreply.github.com>
2025-04-03 09:02:40 +0530

    feat: Add Initial support for 'swarm' backend in schema ,cli and core extensions (#149)

--- src/sugar/utils.py
+++ src/sugar/utils.py
@@ -29,3 +29,25 @@ def camel_to_snake(name: str, sep: str = '-') -> str:
 def get_absolute_path(relative_path: str) -> str:
     """Get the root path of the project."""
     return str(Path(relative_path).resolve())
+# prepend stack name to the services list and stack is should be provided
+# if not throw error
+def prepend_stack_name(stack_name: str, services: list[str]) -> list[str]:
+    """
+    Prepend stack name to the services list.
+
+    Parameters
+    ----------
+    stack_name : str
+        The name of the stack.
+
+    services : list[str]
+        The list of services.
+
+    Returns
+    -------
+    list[str]
+        The list of services with the stack name prepended.
+    """
+    if not stack_name:
+        raise ValueError('Stack name is needed')
+    return [f'{stack_name}_{service}' for service in services]
