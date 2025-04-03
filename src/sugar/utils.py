f44be3ec1581056cf32da1672cd4b3612ba1508c
sanjay7178 <saisanjay7660@gmail.com>
2025-04-03 12:20:40 +0530

    fix : CI linting issue

--- src/sugar/utils.py
+++ src/sugar/utils.py
@@ -30,6 +30,7 @@ def get_absolute_path(relative_path: str) -> str:
     """Get the root path of the project."""
     return str(Path(relative_path).resolve())
 
+
 # prepend stack name to the services list and stack is should be provided
 # if not throw error
 def prepend_stack_name(stack_name: str, services: list[str]) -> list[str]:
